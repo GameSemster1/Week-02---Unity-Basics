5c723c055973db04c6db2edf885bb619d1694791
Elisha <elishadar@gmail.com>
2020-10-31 22:11:50 +0200

    Added Oscillator

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: add documentation
+public class Oscillator : MonoBehaviour
+{
+	public float limit = 1;
+	public float topSpeed = 1;
+
+	private float startX;
+	private float currentAcceleration;
+	private float dircetion;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		startX = transform.position.x;
+		dircetion = 0;
+		transform.position += Vector3.right * limit;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.x < startX)
+			currentAcceleration = 1 / limit;
+		else
+			currentAcceleration = -1 / limit;
+
+		dircetion += currentAcceleration * Time.deltaTime;
+
+		transform.position += new Vector3(dircetion * topSpeed * limit * Time.deltaTime, 0, 0);
+	}
+}
