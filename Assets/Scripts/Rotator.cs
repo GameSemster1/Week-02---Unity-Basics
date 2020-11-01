b468d1b682ce6d67d828abd507a874fd61c0c872
emmanuel405 <emmanuel20.12@gmail.com>
2020-11-01 13:33:13 +0200

    added Rotator

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rottator : MonoBehaviour
+{
+	[Tooltip("speed of rotation")]
+    [SerializeField]
+    float speed = 10f;
+    
+	void Update() {
+		transform.Rotate(0 , speed * Time.deltaTime, 0);
+	}
+}
