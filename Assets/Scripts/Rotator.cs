7d99e7d102b33afe8307823bf996fc0ef1a5ea5f
Elisha <elishadar@gmail.com>
2020-11-01 14:05:01 +0200

    Added Scaler

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rottator : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
-	[Tooltip("speed of rotation")]
-    [SerializeField]
-    float speed = 10f;
-    
-	void Update() {
-		transform.Rotate(0 , speed * Time.deltaTime, 0);
+	[Tooltip("speed of rotation")] [SerializeField]
+	float speed = 10f;
+
+	void Update()
+	{
+		transform.Rotate(0, 0, speed * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour
+{
+	public Vector3 maxScale = new Vector3(1.5f, 1.5f, 1.5f);
+	public Vector3 minScale = new Vector3(0.5f, 0.5f, 0.5f);
+	public Vector3 scaleSpeed = new Vector3(1, 1, 1);
+
+	private bool scaleUp = true;
+
+	// Update is called once per frame
+	void Update()
+	{
+		var currentScale = transform.localScale;
+		if (currentScale.x > maxScale.x || currentScale.y > maxScale.y || currentScale.z > maxScale.z)
+		{
+			scaleUp = false;
+		}
+
+		if (currentScale.x < minScale.x || currentScale.y < minScale.y || currentScale.z < minScale.z)
+		{
+			scaleUp = true;
+		}
+
+		transform.localScale += scaleSpeed * ((scaleUp ? 1 : -1) * Time.deltaTime);
+	}
+}
