3303a271f7f9e2cfc9382546e4c6ba95d599b4f2
Elisha <elishadar@gmail.com>
2020-11-01 17:31:40 +0200

    Started part B

---
+++ Assets/Part B/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public bool upIsForward = true;
+	public float speed = 10;
+
+	public KeyCode forward = KeyCode.UpArrow,
+		back = KeyCode.DownArrow,
+		right = KeyCode.RightArrow,
+		left = KeyCode.LeftArrow;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(forward))
+			transform.Translate((upIsForward ? Vector3.up : Vector3.forward) * (speed * Time.deltaTime));
+
+		if (Input.GetKey(back))
+			transform.Translate(-(upIsForward ? Vector3.up : Vector3.forward) * (speed * Time.deltaTime));
+
+		if (Input.GetKey(right))
+			transform.Translate(Vector3.right * (speed * Time.deltaTime));
+
+		if (Input.GetKey(left))
+			transform.Translate(Vector3.left * (speed * Time.deltaTime));
+	}
+}
