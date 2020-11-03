7ddf498bbe5a0053e8b23e361ed3e05813c04270
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-11-03 18:27:45 +0200

    Part A completed

---
+++ Assets/Part A/Scripts/mouseClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseClick : MonoBehaviour
+{
+    public Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = gameObject.GetComponent<Renderer>();
+        r.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (r.enabled)
+            {
+                r.enabled = false;
+            }
+            else
+            {
+                r.enabled = true;
+            }
+            
+        }
+    }   
+}
